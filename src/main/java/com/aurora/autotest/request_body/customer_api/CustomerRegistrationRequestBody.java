19ae665cf629b1a495a2d4081e2c282930451198
Rurutia1027 <rurutia1027@gmail.com>
2025-12-15 13:16:07 +0800

    add customer registeration requesy body in json obj and try best avoid introduce business layer codes to bdd

--- src/main/java/com/aurora/autotest/request_body/customer_api/CustomerRegistrationRequestBody.java
+++ src/main/java/com/aurora/autotest/request_body/customer_api/CustomerRegistrationRequestBody.java
@@ -4,25 +4,12 @@ import com.alibaba.fastjson.JSONObject;
 /**
  * Helper class to bulid JSON request bodies
  */
-public class CustomerRequestBody {
-    /**
-     * Build request body for creating a new customer.
-     *
-     * @param name Customer's name
-     * @param email Customer's email
-     * @return  JSON string ready to send via REST call
-     */
-    public String buildCreateCustomerRequestyBody(String name, String email) {
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("name", name);
-        jsonObject.put("email", email);
-
-        return jsonObject.toString();
+public class CustomerRegistrationRequestBody {
+    public static String build(String firstName, String lastName, String email) {
+        JSONObject json = new JSONObject();
+        json.put("firstName", firstName);
+        json.put("lastName", lastName);
+        json.put("email", email);
+        return json.toJSONString();
     }
-
-    /**
-     * Build request body for updating an existing customer.
-     *
-     * @param customerId ID of customer to update
-     */
 }
